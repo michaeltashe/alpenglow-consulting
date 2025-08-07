2526ddc245e09c2e2a6d64bcbcf7a438603d5f5f
Michael Ashe <michaeltashe@gmail.com>
2025-08-07 17:35:47 -0400

    Test commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Theme } from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme appearance="dark" accentColor="green" grayColor="olive">{children}<ThemePanel /></Theme>
+        <Theme appearance="dark" accentColor="green" grayColor="olive">{children}</Theme>
       </body>
     </html>
   );
