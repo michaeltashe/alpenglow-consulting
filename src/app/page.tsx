543b4ce88e314f32dcdcf3f4f17afbba997886b9
michaeltashe <michaeltashe@gmail.com>
2025-08-07 20:35:19 -0400

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,41 @@
 'use client'
 
-import { Button } from "@radix-ui/themes";
+import { Avatar, Box, Button, Card, Flex, Heading, Text } from "@radix-ui/themes";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div>Michael Ashe</div>
-        <Button onClick={() => alert("Hello Jordan")}>Test</Button>
+    <div className="8font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col items-start">
+        <Heading style={{marginTop: "400px"}}>2Freakz</Heading>
+        <Text>The best thing since sliced bread.</Text>
+        <Button className="cursor-pointer" onClick={() => alert("Hello Jordan")}>Learn more</Button>
+
+        <Heading style={{marginTop: "32px"}} size="4">Meet the Team</Heading>
+        <Flex gap="3" direction="column">
+        <Box>
+          <Card>
+            <Flex gap="3" align="center">
+              <Avatar src="https://media.licdn.com/dms/image/v2/D4E03AQEVdIML0AQvgQ/profile-displayphoto-shrink_200_200/B4EZb_yvqEHQAY-/0/1748048214923?e=2147483647&v=beta&t=_Nnj6wNeCHZ3KAb7bKVycCbj0UNQHh0H4G4geVQExmQ" radius="full" size="3" fallback="JM"/>
+              <Box>
+                <Text as="div" weight="bold" size="2">Jordan Miller</Text>
+                <Text as="div" color="gray" size="2">ML Engineer at Crimson Phoenix</Text>
+              </Box>
+            </Flex>
+          </Card>
+        </Box>
+        <Box>
+          <Card>
+            <Flex gap="3" align="center">
+              <Avatar src="https://media.licdn.com/dms/image/v2/D4E03AQF0gdFu7Za9dA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1637720480289?e=2147483647&v=beta&t=b_LuCFAXFCa30lX0le-QC-RYPw3bQAsNUOjFMh7FY_E" radius="full" size="3" fallback="MA"/>
+              <Box>
+                <Text as="div" weight="bold" size="2">Michael Ashe</Text>
+                <Text as="div" color="gray" size="2">Associate Software Engineer at Lowe's</Text>
+              </Box>
+            </Flex>
+          </Card>
+        </Box>
+        </Flex>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
