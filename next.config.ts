bf0841b2440b44f0156947bda7743ad9ebb4aac8
Michael Ashe <michaeltashe@gmail.com>
2025-08-08 23:42:07 -0400

    build fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import withPWA from 'next-pwa'
 const path = require('path')
 
-const nextConfig: NextConfig = {
+const nextConfig: any = {
   /* config options here */
   reactStrictMode: true,
   compiler: {
